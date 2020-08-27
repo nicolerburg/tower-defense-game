da02985aadcd4f8b753e4cf212d33b55257ed02d
Nicole Burg <32913338+nicolerburg@users.noreply.github.com>
2020-08-27 19:22:12 -0400

    Moving player with mouse click

---
+++ Assets/Scripts/MouseInput.cs
@@ -0,0 +1,161 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Mouse.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @MouseInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @MouseInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Mouse"",
+    ""maps"": [
+        {
+            ""name"": ""Mouse"",
+            ""id"": ""aa87e3ed-251f-43bc-a960-559d5f6c14d6"",
+            ""actions"": [
+                {
+                    ""name"": ""Click"",
+                    ""type"": ""Button"",
+                    ""id"": ""f1826d78-524a-4d15-884f-462726aa2736"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Position"",
+                    ""type"": ""Value"",
+                    ""id"": ""30674ac8-5a1d-4728-8b38-bec8425bc973"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""9cc1d295-934a-45e0-8cc5-a9d8c4e4d411"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Click"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""7a6f5f6e-5036-41bd-9ac8-e7b4d600b7b6"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Position"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Mouse
+        m_Mouse = asset.FindActionMap("Mouse", throwIfNotFound: true);
+        m_Mouse_Click = m_Mouse.FindAction("Click", throwIfNotFound: true);
+        m_Mouse_Position = m_Mouse.FindAction("Position", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Mouse
+    private readonly InputActionMap m_Mouse;
+    private IMouseActions m_MouseActionsCallbackInterface;
+    private readonly InputAction m_Mouse_Click;
+    private readonly InputAction m_Mouse_Position;
+    public struct MouseActions
+    {
+        private @MouseInput m_Wrapper;
+        public MouseActions(@MouseInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Click => m_Wrapper.m_Mouse_Click;
+        public InputAction @Position => m_Wrapper.m_Mouse_Position;
+        public InputActionMap Get() { return m_Wrapper.m_Mouse; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MouseActions set) { return set.Get(); }
+        public void SetCallbacks(IMouseActions instance)
+        {
+            if (m_Wrapper.m_MouseActionsCallbackInterface != null)
+            {
+                @Click.started -= m_Wrapper.m_MouseActionsCallbackInterface.OnClick;
+                @Click.performed -= m_Wrapper.m_MouseActionsCallbackInterface.OnClick;
+                @Click.canceled -= m_Wrapper.m_MouseActionsCallbackInterface.OnClick;
+                @Position.started -= m_Wrapper.m_MouseActionsCallbackInterface.OnPosition;
+                @Position.performed -= m_Wrapper.m_MouseActionsCallbackInterface.OnPosition;
+                @Position.canceled -= m_Wrapper.m_MouseActionsCallbackInterface.OnPosition;
+            }
+            m_Wrapper.m_MouseActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Click.started += instance.OnClick;
+                @Click.performed += instance.OnClick;
+                @Click.canceled += instance.OnClick;
+                @Position.started += instance.OnPosition;
+                @Position.performed += instance.OnPosition;
+                @Position.canceled += instance.OnPosition;
+            }
+        }
+    }
+    public MouseActions @Mouse => new MouseActions(this);
+    public interface IMouseActions
+    {
+        void OnClick(InputAction.CallbackContext context);
+        void OnPosition(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement_Iso.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement_Iso : MonoBehaviour
+{
+    MouseInput mouseInput;
+
+    [Header("Movement Variables")]
+    public float walkSpeed = 5;
+
+    [HideInInspector]
+    //public bool frozen = false;
+    //private float inputHorizontal;
+    //private float inputVertical;
+    private Vector3 destination;
+    public Animator animator;
+
+    private Rigidbody2D rigidBody;
+    //private SpriteRenderer spriteRenderer;
+    //private Collider2D collider;
+
+    private void Awake() {
+        mouseInput = new MouseInput();
+    }
+
+    private void OnEnable() {
+        mouseInput.Enable();
+    }
+
+    private void OnDisable() {
+        mouseInput.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        destination = transform.position;
+        mouseInput.Mouse.Click.performed += _ => FindPosition();
+        //rigidBody = GetComponent<Rigidbody2D>();
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+        //collider = GetComponent<Collider2D>();
+    }
+
+    private void FindPosition() {
+        UnityEngine.Debug.Log("We Clicked!");
+        Vector2 mousePosition = mouseInput.Mouse.Position.ReadValue<Vector2>();
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        destination = mousePosition;
+    }
+
+    void FixedUpdate() {
+        //inputHorizontal = Input.GetAxis("Horizontal");
+        //inputVertical = Input.GetAxis("Vertical");
+        //rigidBody.velocity = new Vector2(inputHorizontal * walkSpeed, inputVertical * walkSpeed);
+        //animator.SetFloat("horSpeed", rigidBody.velocity.x);
+        //animator.SetFloat("vertSpeed", rigidBody.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, destination) > 0.1f) {
+            transform.position = Vector3.MoveTowards(transform.position, destination, walkSpeed * Time.deltaTime);
+        }
+    }
+}
